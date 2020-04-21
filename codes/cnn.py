53934d4a3588abd38d2d7cfd978ba3e711ee141d
Eirini Cholopoulou <reanacc13@gmail.com>
2020-04-21 19:14:27 +0300

    prog_1

--- codes/cnn.py
+++ codes/cnn.py
@@ -1,6 +1,4 @@
 """
-Created on Sun Feb 10 16:01:36 2019
-
 @author: Rena
 """
 
@@ -19,10 +17,10 @@ from keras.layers import Dropout
 # Initialising the CNN
 model = Sequential() 
 
-#Convolution
+# Step 1 - Convolution
 model.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu'))
 
-#Pooling
+# Step 2 - Pooling
 model.add(MaxPooling2D(pool_size = (2, 2))) 
 
 #2nd layer
@@ -33,23 +31,32 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Conv2D(64, (3, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
-#for 6th model
+#for 6th model:
 model.add(Dropout(0.3))
 
-#Flattening
+# Step 3 - Flattening
 model.add(Flatten())#no pars ,it flattens the previous layer
 
-#Full connection
-model.add(Dense(units = 128, activation = 'relu')) #unis =output dimension
+# Step 4 - Full connection
+model.add(Dense(units = 128, activation = 'relu')) #128 is up for exp/tion ,units=output dimension
+#model.add(Dense(units = 64, activation = 'relu'))
 model.add(Dropout(0.5))
-model.add(Dense(units = 1, activation = 'sigmoid')) #final layer,sigmoid->binary outcome
+#model.add(Dense(units = 64, activation = 'relu'))
+model.add(Dense(units = 1, activation = 'sigmoid')) #output layer,sigmoid->binary outcome, else softmax
+'''
+classifier.add(Dense(units=64, activation='relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(units=64, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+'''
 # Compiling the CNN
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-#code from keras_documentation
+#code form keras_documentation
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,  #geometrical transformation
+                                   shear_range = 0.2,  
                                    zoom_range = 0.2,
                                    horizontal_flip = True) 
 
@@ -64,9 +71,11 @@ test_set = test_datagen.flow_from_directory('C:/Users/Rena/Desktop/Sxolh/Πτυ�
                                             target_size = (128, 128),
                                             batch_size = 32,
                                             class_mode = 'binary')
+
 model.summary()
-model.fit_generator( training_set, 
-                     steps_per_epoch = 465,
-                     epochs = 4, 
-                     validation_data = test_set,
-                     validation_steps = 233) 
+
+model.fit_generator(training_set, 
+                         steps_per_epoch = 465,
+                         epochs = 4, 
+                         validation_data = test_set,
+                         validation_steps = 233) 
