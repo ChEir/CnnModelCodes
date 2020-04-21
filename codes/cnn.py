716c069fdf60dad597d19034749223b97f93edd3
Eirini Cholopoulou <reanacc13@gmail.com>
2020-04-21 17:26:40 +0300

    prototype

--- codes/cnn.py
+++ codes/cnn.py
@@ -1,19 +1,15 @@
 """
-Created on Sun Feb 10 16:01:36 2019
-
 @author: Rena
 """
-
 # Importing the Keras libraries and packages
-
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np 
-from keras.preprocessing import image
+import numpy as np                    #for predicting
+from keras.preprocessing import image #for predicting
 from keras.layers import Dropout
 
 # Initialising the CNN
@@ -43,30 +39,54 @@ model.add(Flatten())#no pars ,it flattens the previous layer
 model.add(Dense(units = 128, activation = 'relu')) #unis =output dimension
 model.add(Dropout(0.5))
 model.add(Dense(units = 1, activation = 'sigmoid')) #final layer,sigmoid->binary outcome
+
 # Compiling the CNN
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-#code from keras_documentation
-
+#Code from keras_documentation
+#Fitting the CNN to the images
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,  #geometrical transformation
                                    zoom_range = 0.2,
                                    horizontal_flip = True) 
 
-test_datagen = ImageDataGenerator(rescale = 1./255) 
+test_datagen = ImageDataGenerator(rescale = 1./255) #augment test set
 
-training_set = train_datagen.flow_from_directory('C:/Users/Rena/Desktop/Sxolh/Πτυχιακη/data/train',
+training_set = train_datagen.flow_from_directory('../data/train',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary') 
 
-test_set = test_datagen.flow_from_directory('C:/Users/Rena/Desktop/Sxolh/Πτυχιακη/data/test',
+test_set = test_datagen.flow_from_directory('../data/test',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 model.summary()
 model.fit_generator( training_set, 
-                     steps_per_epoch = 465,
+                     steps_per_epoch = 465, #num of images in train set
                      epochs = 4, 
                      validation_data = test_set,
-                     validation_steps = 233) 
+                     validation_steps = 233) ##num of images in test set
+
+
+
+
+
+
+
+
+
+
+
+test_im = image.load_img('path_of_new_image', target_size=(64,64)) #same targ_size 
+test_im = image.img_to_array(test_im)
+#add one more dim , to be able to use the predict method 
+test_im = np.expand_dims(test_im, axis =0 )
+result= model.predict(test_im)
+
+# training_Set.class_indices #shows what corresponds to what 
+if result[0][0] == 1:
+     prediction ='normal'
+else:
+    prediction = 'abnormal'
+
